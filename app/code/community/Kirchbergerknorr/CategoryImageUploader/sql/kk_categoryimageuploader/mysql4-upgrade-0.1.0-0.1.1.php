f974bcf5014d000669b69f36476c16153852a6f1
Aleksey Razbakov <ar@kirchbergerknorr.de>
2014-11-11 17:03:14 +0100

    remove catalog/category_attribute_backend_image from eav_attribute

---
+++ app/code/community/Kirchbergerknorr/CategoryImageUploader/sql/kk_categoryimageuploader/mysql4-upgrade-0.1.0-0.1.1.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Installer
+ *
+ * @category    Kirchbergerknorr
+ * @package     Kirchbergerknorr
+ * @author      Aleksey Razbakov <ar@kirchbergerknorr.de>
+ * @copyright   Copyright (c) 2014 kirchbergerknorr GmbH (http://www.kirchbergerknorr.de)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/* @var $installer Mage_Sales_Model_Entity_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->run(sprintf(
+    "DELETE FROM `%s` WHERE `backend_model` = 'catalog/category_attribute_backend_image'",
+    $this->getTable('eav/attribute')
+));
+
+$installer->endSetup();
